2c74c71752c190a063336005c4e725e43c842ab1
SofKh <sofiane.chkh@gmail.com>
2022-03-07 10:37:22 -0500

    Create exercice3.py

---
+++ exercice3.py
@@ -0,0 +1,6 @@
+jour_fete = int(input("Veuillez entrer votre jour de fete : "))
+mois_fete = int(input("Veuillez entrer votre mois de fete en chiffre :"))
+if jour_fete == 7 and mois_fete == 3 :
+    print("bonne fete")
+else:
+    print("Ce n'est pas ta fete aujourd'hui :( ")
